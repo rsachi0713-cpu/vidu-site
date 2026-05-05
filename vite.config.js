2c7a814d7e1b16ca1f5291096637db1e69883196
Tharindu Thilakarathne <rsachi0713@gmail.com>
2026-05-05 15:50:37 +0530

    update 5

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [],
   build: {
     outDir: 'dist',
-  }
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), 'index.html'),
+        admin: resolve(process.cwd(), 'admin.html'),
+      },
+    },
+  },
 })
